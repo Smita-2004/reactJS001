88e13762d4b0a74f0e3c81ae02e19c0a861e1440
Smita-2004 <smita23tiwari.007@gmail.com>
2026-04-17 11:48:49 +0530

    commit

---
+++ App.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+/*
+Header
+ - logo
+ - nav items
+ Body
+  - search
+  - item cards
+ Footer
+  - copy right
+  - links
+  - contact
+
+*/
+
+const Header = () =>{
+    return(
+    <div className="header">
+
+        <div className="logo-container">
+            <img className="logo" src="https://imgcdn.stablediffusionweb.com/2025/4/14/116e900c-24b3-41f9-be63-ffa9dcdfcdb2.jpg"></img>
+        </div>
+
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>cart</li>
+            </ul>
+        </div>
+    </div>
+    );
+};
+
+const Body = () =>{
+    return (
+        <h1>This is body</h1>
+    );
+};
+
+const Footer = () =>{
+    return(
+    <h1>This is Footer</h1>
+    );
+};
+
+const AppLayout = () =>{
+    return (
+        <div className = "app">
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>);
